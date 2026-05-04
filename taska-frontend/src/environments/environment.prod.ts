02373927ce063534df99210482a09f40f3196ca3
Christian German <christian.german@pm.me>
2026-05-05 00:45:58 +0200

    chore: remove Authentik middleware from Traefik routes and standardize quotes in production environment configuration

--- taska-frontend/src/environments/environment.prod.ts
+++ taska-frontend/src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
-  apiUrl: "https://api-taska.atlascore.dev",
+  apiUrl: 'https://api-taska.atlascore.dev',
   oidc: {
-    authority: "https://authentik.atlascore.dev/application/o/taska",
-    "clientId": "mE2vXI67I43D8fmclgsjHKwt42W4dkDpXJQUOQEJ",
-    "redirectUri": "https://taska.atlascore.dev/callback",
-    "postLogoutRedirectUri": "https://taska.atlascore.dev",
-    "scope": "openid profile email"
+    authority: 'https://authentik.atlascore.dev/application/o/taska',
+    clientId: 'mE2vXI67I43D8fmclgsjHKwt42W4dkDpXJQUOQEJ',
+    redirectUri: 'https://taska.atlascore.dev/callback',
+    postLogoutRedirectUri: 'https://taska.atlascore.dev',
+    scope: 'openid profile email'
   }
 };

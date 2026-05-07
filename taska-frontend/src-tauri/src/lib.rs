b86c705f5bda5816b4c37a9ff4512d1cd9f2ac76
Christian German <christian.german@pm.me>
2026-05-08 00:47:16 +0200

    chore: integrate tauri-plugin-process and update default capabilities configuration

--- taska-frontend/src-tauri/src/lib.rs
+++ taska-frontend/src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ pub fn run() {
     .plugin(tauri_plugin_notification::init())
     .plugin(tauri_plugin_deep_link::init())
     .plugin(tauri_plugin_updater::Builder::new().build())
+    .plugin(tauri_plugin_process::init())
     .plugin(
       tauri_plugin_log::Builder::new()
         .targets([

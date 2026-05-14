59b67d98e836a71eb3d08715d3ff814f8725c232
Christian German <christian.german@pm.me>
2026-05-14 12:17:22 +0200

    chore: update Firebase config to use `@Value` for service account resource path

--- taska-backend/src/main/java/com/taska/config/FirebaseConfig.java
+++ taska-backend/src/main/java/com/taska/config/FirebaseConfig.java
@@ -4,20 +4,22 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 import java.io.IOException;
-import java.io.InputStream;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${firebase.service-account-path:classpath:firebase-service-account.json}")
+    private Resource serviceAccountResource;
+
     @PostConstruct
     public void init() throws IOException {
-        InputStream serviceAccount = new ClassPathResource("firebase-service-account.json").getInputStream();
         FirebaseOptions options = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .setCredentials(GoogleCredentials.fromStream(serviceAccountResource.getInputStream()))
                 .build();
         FirebaseApp.initializeApp(options);
     }
